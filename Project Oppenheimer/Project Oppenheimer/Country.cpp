dfc85d963a371349a8f2084ac3beb7414a49b847
Noah Thederahn <TrikkStar@users.noreply.github.com>
2015-12-29 21:08:04 -0500

    added shells for country methods

--- Project Oppenheimer/Project Oppenheimer/Country.cpp
+++ Project Oppenheimer/Project Oppenheimer/Country.cpp
@@ -10,5 +10,63 @@ Country::Country(std::string n, int s, int i, std::vector<int> vect)
 	std::vector<int> adjacent = vect;
 	influenceUSA = 0;
 	influenceUSSR = 0;
+	controlled = false;
 	controller = "NONE";
+}
+
+
+int Country::get_stability()
+{
+	return 0;
+}
+
+
+std::string Country::get_name()
+{
+	return std::string();
+}
+
+
+int Country::get_id()
+{
+	return 0;
+}
+
+
+std::vector<int> Country::get_adjacent()
+{
+	return std::vector<int>();
+}
+
+
+std::vector<int> Country::get_influence()
+{
+	return std::vector<int>();
+}
+
+
+bool Country::is_controlled()
+{
+	return false;
+}
+
+
+std::string Country::get_controller()
+{
+	return std::string();
+}
+
+
+void Country::set_infUSA(int x)
+{
+}
+
+
+void Country::set_infUSSR(int x)
+{
+}
+
+
+void Country::updateControl()
+{
 }
--- Project Oppenheimer/Project Oppenheimer/Country.h
+++ Project Oppenheimer/Project Oppenheimer/Country.h
@@ -15,5 +15,17 @@ private:
 	std::string name;
 	int id;
 	std::vector<int> adjacent;
+public:
+	int get_stability();
+	std::string get_name();
+	int get_id();
+	std::vector<int> get_adjacent();
+	std::vector<int> get_influence();
+	bool is_controlled();
+	std::string get_controller();
+	void set_infUSA(int x);
+	void set_infUSSR(int x);
+private:
+	void updateControl();
 };
 
