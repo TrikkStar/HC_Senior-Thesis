f30c531d529f7fa93c8ccef5595cacdb9e69c25b
Noah Thederahn <TrikkStar@users.noreply.github.com>
2016-02-15 16:30:08 -0500

    added tracking to archived project

---
+++ refrences/personal_resources/Archived Project/Project Oppenheimer/main.cpp
@@ -0,0 +1,160 @@
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+#include "resource.h"
+
+// Global variables
+
+// The main window class name.
+static TCHAR szWindowClass[] = _T("ProjectOppenheimerapp");
+
+// The string that appears in the application's title bar.
+static TCHAR szTitle[] = _T("Project Oppenheimer");
+
+HINSTANCE hInst;
+
+// Forward declarations of functions included in this code module:
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+
+	if (!RegisterClassEx(&wcex))
+	{
+		MessageBox(NULL,
+			_T("Call to RegisterClassEx failed!"),
+			_T("ProjectOppenheimerapp"),
+			NULL);
+
+		return 1;
+	}
+
+	hInst = hInstance; // Store instance handle in our global variable
+
+					   // The parameters to CreateWindow explained:
+					   // szWindowClass: the name of the application
+					   // szTitle: the text that appears in the title bar
+					   // WS_OVERLAPPEDWINDOW: the type of window to create
+					   // CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
+					   // 500, 100: initial size (width, length)
+					   // NULL: the parent of this window
+					   // NULL: this application does not have a menu bar
+					   // hInstance: the first parameter from WinMain
+					   // NULL: not used in this application
+	HWND hWnd = CreateWindow(
+		szWindowClass,
+		szTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		500, 100,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+		);
+
+	if (!hWnd)
+	{
+		MessageBox(NULL,
+			_T("Call to CreateWindow failed!"),
+			_T("ProjectOppenheimerapp"),
+			NULL);
+
+		return 1;
+	}
+
+	// The parameters to ShowWindow explained:
+	// hWnd: the value returned from CreateWindow
+	// nCmdShow: the fourth parameter from WinMain
+	ShowWindow(hWnd,
+		nCmdShow);
+	UpdateWindow(hWnd);
+
+	// Main message loop:
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
+	switch (message)
+	{
+	case WM_PAINT:
+		{
+		PAINTSTRUCT ps;
+		HDC hdc;
+		TCHAR greeting[] = _T("Now I am become Death, the destroyer of worlds.");
+
+		hdc = BeginPaint(hWnd, &ps);
+		TextOut(hdc,
+			5, 5,
+			greeting, _tcslen(greeting));
+
+		EndPaint(hWnd, &ps);
+		}
+		break;
+
+	/*case WM_RBUTTONDOWN:
+		{
+			
+		}
+		break;*/
+
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+			case ID_DEVELOPERTOOLS_TEST:
+				TCHAR szFileName[MAX_PATH];
+				HINSTANCE hInstance = GetModuleHandle(NULL);
+
+				GetModuleFileName(hInstance, szFileName, MAX_PATH);
+				MessageBox(hWnd, szFileName, _T("This program is:"), MB_OK | MB_ICONINFORMATION);
+			break;
+		}
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+		break;
+	}
+
+	return 0;
+}
