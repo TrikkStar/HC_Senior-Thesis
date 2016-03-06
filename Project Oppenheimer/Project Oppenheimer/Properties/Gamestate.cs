58e84d10a8247cf87df55de8c131a2b2bd60cd3f
Noah Thederahn <TrikkStar@users.noreply.github.com>
2016-03-06 16:08:06 -0500

    remade gamestate

--- Project Oppenheimer/Project Oppenheimer/Properties/Gamestate.cs
+++ Project Oppenheimer/Project Oppenheimer/Properties/Gamestate.cs
@@ -8,5 +8,21 @@ namespace Project_Oppenheimer.Properties
 {
     class Gamestate
     {
+        public CountryList countryLst;
+        public CardList cards;
+        public int score;
+        public int defcon;
+        public int turn;
+        public int round;
+
+        public Gamestate()
+        {
+            score = 0;
+            defcon = 5;
+            turn = 0;
+            round = 0;
+            countryLst = new CountryList();
+            cards = new CardList();
+        }
     }
 }
