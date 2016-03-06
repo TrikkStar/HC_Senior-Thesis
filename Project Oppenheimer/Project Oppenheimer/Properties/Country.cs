75ccd0c482d73ac14f111ed043786d8e1c311123
Noah Thederahn <thederahnn@gmail.com>
2016-03-05 22:00:56 -0500

    working on Country.cs

--- Project Oppenheimer/Project Oppenheimer/Properties/Country.cs
+++ Project Oppenheimer/Project Oppenheimer/Properties/Country.cs
@@ -8,5 +8,27 @@ namespace Project_Oppenheimer.Properties
 {
     class Country
     {
+        public string name;
+        public int stability;
+        public int id;
+        public bool battleground;
+        public List<int> adjacent;
+        public int influenceUSA;
+        public int influenceUSSR;
+        public bool controlled;
+        public int controller;
+
+        public Country(string n, int i, int s, bool b, List<int> l)
+        {
+            name = n;
+            id = i;
+            stability = s;
+            battleground = b;
+            adjacent = l;
+            influenceUSA = 0;
+            influenceUSSR = 0;
+            controlled = false;
+            controller = 0;
+        }
     }
 }
