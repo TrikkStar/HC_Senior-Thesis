3765db78aeee429171d8ee03baeb015d1cdfd955
Noah Thederahn <TrikkStar@users.noreply.github.com>
2016-03-06 16:22:08 -0500

    skelaton for cardList

--- Project Oppenheimer/Project Oppenheimer/Properties/CardList.cs
+++ Project Oppenheimer/Project Oppenheimer/Properties/CardList.cs
@@ -8,5 +8,58 @@ namespace Project_Oppenheimer.Properties
 {
     class CardList
     {
+        public List<Card> deck;
+        public List<Card> discarded;
+        public List<Card> removed;
+        public List<Card> usHand;
+        public List<Card> ussrHand;
+        public List<Card> earlyWar;
+        public List<Card> midWar;
+        public List<Card> lateWar;
+
+        public CardList()
+        {
+
+        }
+
+        public void deal(int x)
+        {
+
+        }
+
+        public void shuffel()
+        {
+
+        }
+
+        public void discard(int x)
+        {
+
+        }
+
+        public void remove(int x)
+        {
+
+        }
+
+        private Card find(int x)
+        {
+            return null;
+        }
+
+        private void init_EW()
+        {
+
+        }
+
+        private void init_MW()
+        {
+
+        }
+
+        public void init_LW()
+        {
+
+        }
     }
 }
