6453df0131cf9e48d5fefb6f42d7c14092bd1f8a
Noah Thederahn <TrikkStar@users.noreply.github.com>
2016-03-11 01:13:48 -0500

    minor cleanup of files

--- Project Oppenheimer/Project Oppenheimer/Properties/Card.cs
+++ Project Oppenheimer/Project Oppenheimer/Properties/Card.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Project_Oppenheimer.Properties
+﻿namespace Project_Oppenheimer.Properties
 {
     struct Card
     {
