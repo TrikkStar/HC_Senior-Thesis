82eb62330fc2c4b507c6adbc245ddc73d4dc78c9
Noah Thederahn <TrikkStar@users.noreply.github.com>
2016-03-16 11:20:11 -0400

    added more scoring functions

--- Project Oppenheimer/Project Oppenheimer/Properties/Gamestate.cs
+++ Project Oppenheimer/Project Oppenheimer/Properties/Gamestate.cs
@@ -230,17 +230,177 @@ namespace Project_Oppenheimer.Properties
 
         private void scoreCentralAmerica()
         {
-
+            int usPresence = 0;
+            int ussrPresence = 0;
+            int usBattlegrounds = 0;
+            int ussrBattlegrounds = 0;
+            foreach (int element in countryLst.CentralAmerica)
+            {
+                var temp = countryLst.countries[element];
+                if (temp.controlled)
+                {
+                    if (temp.controller == -1)
+                    {
+                        ussrPresence++;
+                        if (temp.battleground)
+                        {
+                            ussrBattlegrounds++;
+                        }
+                    }
+                    else if (temp.controller == 1)
+                    {
+                        usPresence++;
+                        if (temp.battleground)
+                        {
+                            usBattlegrounds++;
+                        }
+                    }
+                }
+            }
+            if ((usBattlegrounds == 3) && (usPresence > ussrPresence) && (usPresence >= 4))
+            {
+                scoreUSA(5);
+            }
+            else if ((usBattlegrounds > ussrBattlegrounds) && (usPresence > ussrPresence) && (usPresence >= 2) && (usBattlegrounds >= 1))
+            {
+                scoreUSA(3);
+            }
+            else if (usPresence >= 1)
+            {
+                scoreUSA(1);
+            }
+            if ((ussrBattlegrounds == 3) && (usPresence < ussrPresence) && (ussrPresence >= 4))
+            {
+                scoreUSSR(5);
+            }
+            else if ((usBattlegrounds < ussrBattlegrounds) && (usPresence < ussrPresence) && (ussrPresence >= 2) && (ussrBattlegrounds >= 1))
+            {
+                scoreUSSR(3);
+            }
+            else if (usPresence >= 1)
+            {
+                scoreUSSR(1);
+            }
+            scoreUSA(usBattlegrounds);
+            scoreUSSR(ussrBattlegrounds);
+            //need to add special cases for adjacent country scoring
+            checkVictory();
         }
 
         private void scoreSouthAmerica()
         {
-
+            int usPresence = 0;
+            int ussrPresence = 0;
+            int usBattlegrounds = 0;
+            int ussrBattlegrounds = 0;
+            foreach (int element in countryLst.SouthAmerica)
+            {
+                var temp = countryLst.countries[element];
+                if (temp.controlled)
+                {
+                    if (temp.controller == -1)
+                    {
+                        ussrPresence++;
+                        if (temp.battleground)
+                        {
+                            ussrBattlegrounds++;
+                        }
+                    }
+                    else if (temp.controller == 1)
+                    {
+                        usPresence++;
+                        if (temp.battleground)
+                        {
+                            usBattlegrounds++;
+                        }
+                    }
+                }
+            }
+            if ((usBattlegrounds == 4) && (usPresence > ussrPresence) && (usPresence >= 5))
+            {
+                scoreUSA(6);
+            }
+            else if ((usBattlegrounds > ussrBattlegrounds) && (usPresence > ussrPresence) && (usPresence >= 2) && (usBattlegrounds >= 1))
+            {
+                scoreUSA(5);
+            }
+            else if (usPresence >= 1)
+            {
+                scoreUSA(2);
+            }
+            if ((ussrBattlegrounds == 4) && (usPresence < ussrPresence) && (ussrPresence >= 5))
+            {
+                scoreUSSR(6);
+            }
+            else if ((usBattlegrounds < ussrBattlegrounds) && (usPresence < ussrPresence) && (ussrPresence >= 2) && (ussrBattlegrounds >= 1))
+            {
+                scoreUSSR(5);
+            }
+            else if (usPresence >= 1)
+            {
+                scoreUSSR(2);
+            }
+            scoreUSA(usBattlegrounds);
+            scoreUSSR(ussrBattlegrounds);
+            checkVictory();
         }
 
         private void scoreAfrica()
         {
-
+            int usPresence = 0;
+            int ussrPresence = 0;
+            int usBattlegrounds = 0;
+            int ussrBattlegrounds = 0;
+            foreach (int element in countryLst.Africa)
+            {
+                var temp = countryLst.countries[element];
+                if (temp.controlled)
+                {
+                    if (temp.controller == -1)
+                    {
+                        ussrPresence++;
+                        if (temp.battleground)
+                        {
+                            ussrBattlegrounds++;
+                        }
+                    }
+                    else if (temp.controller == 1)
+                    {
+                        usPresence++;
+                        if (temp.battleground)
+                        {
+                            usBattlegrounds++;
+                        }
+                    }
+                }
+            }
+            if ((usBattlegrounds == 5) && (usPresence > ussrPresence) && (usPresence >= 6))
+            {
+                scoreUSA(6);
+            }
+            else if ((usBattlegrounds > ussrBattlegrounds) && (usPresence > ussrPresence) && (usPresence >= 2) && (usBattlegrounds >= 1))
+            {
+                scoreUSA(4);
+            }
+            else if (usPresence >= 1)
+            {
+                scoreUSA(1);
+            }
+            if ((ussrBattlegrounds == 5) && (usPresence < ussrPresence) && (ussrPresence >= 6))
+            {
+                scoreUSSR(6);
+            }
+            else if ((usBattlegrounds < ussrBattlegrounds) && (usPresence < ussrPresence) && (ussrPresence >= 2) && (ussrBattlegrounds >= 1))
+            {
+                scoreUSSR(4);
+            }
+            else if (usPresence >= 1)
+            {
+                scoreUSSR(1);
+            }
+            scoreUSA(usBattlegrounds);
+            scoreUSSR(ussrBattlegrounds);
+            checkVictory();
         }
 
         private void scoreMiddleEast()
